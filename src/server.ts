138be1a2b3edead1be3653706744805f5f906c2c
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-04-04 15:02:17 -0300

    feat: add new routes to : register the attendee on an event, get the attendee badge, and get the events.

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,9 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 const app = fastify();
 // Add schema validator and serializer
@@ -12,6 +15,9 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
 
 app
   .listen({
